afc69f494971fc51b3efd3850543107e831f1194
tchisama <tchisama41@gmail.com>
2023-12-24 11:21:16 +0100

    lets gooo

---
+++ firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import database 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBOvM-Sla6qAuMmhZH354UAE4OtwYvb_4w",
+  authDomain: "easy-backend-42237.firebaseapp.com",
+  projectId: "easy-backend-42237",
+  storageBucket: "easy-backend-42237.appspot.com",
+  messagingSenderId: "554556467851",
+  appId: "1:554556467851:web:a61565cc9523a00128828e",
+  measurementId: "G-P77EJXVDXC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// database reference
+
